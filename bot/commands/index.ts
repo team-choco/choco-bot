68cedf7e9def864a06572d367f4fe4e744eeb7f2
Cecilia Woodward <ceci@salte.io>
2021-01-13 15:22:56 -0600

    chore: more lodestone references

--- bot/commands/index.ts
+++ bot/commands/index.ts
@@ -2,10 +2,8 @@ import { ChocoBotCommand } from './types';
 
 import * as DebugCommands from './debug';
 import * as ReactCommands from './react';
-// import * as LodestoneCommands from './lodestone';
 
 export const COMMANDS: ChocoBotCommand[] = [
   ...Object.values(DebugCommands),
   ...Object.values(ReactCommands),
-  // ...Object.values(LodestoneCommands),
 ];
